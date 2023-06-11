15e8729d24888d133440b72cecd2da979282a9af
Konstantin Kostov <kosta.norge@gmail.com>
2023-06-11 23:50:56 +0300

    change container name

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
   backend "azurerm" {
     resource_group_name = "StorageRG"
     storage_account_name = "taskboardstoragekoss"
-    container_name = "taskboardcontainerkoss"
+    container_name = "taskboardcontainer2"
     key = "terraform.tfstate"
   }
 }
